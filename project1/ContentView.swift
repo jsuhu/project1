aaad31637daee62d780b240a75b437a872cb6a09
jsuhu <111390114+jsuhu@users.noreply.github.com>
2023-12-08 11:31:51 -0500

    Initial Commit

---
+++ project1/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  project1
+//
+//  Created by Jingyi Su on 12/8/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ project1/project1App.swift
@@ -0,0 +1,17 @@
+//
+//  project1App.swift
+//  project1
+//
+//  Created by Jingyi Su on 12/8/23.
+//
+
+import SwiftUI
+
+@main
+struct project1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

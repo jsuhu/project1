a4ee17f5b90d482aac7b9e6d7a9bcdf6f3b9add1
jsuhu <111390114+jsuhu@users.noreply.github.com>
2023-12-11 12:05:51 -0500

    Edit content with let content

--- project1/ContentView.swift
+++ project1/ContentView.swift
@@ -10,10 +10,10 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         HStack {
-            CardView(isFaceUp: true)
-            CardView(isFaceUp: true)
-            CardView()
-            CardView()
+            CardView(content: "👻", isFaceUp: true)
+            CardView(content: "🎃")
+            CardView(content: "😈")
+            CardView(content: "🕷️")
         }
         .foregroundColor(.orange)
         .padding()
@@ -21,6 +21,7 @@ struct ContentView: View {
 }
 
 struct CardView: View {
+    let content: String
     @State var isFaceUp = false
      
     var body: some View {
@@ -29,7 +30,7 @@ struct CardView: View {
             if isFaceUp {
                 base.fill(.white)
                 base.strokeBorder(lineWidth: 2)
-                Text("👻").font(.largeTitle)
+                Text(content).font(.largeTitle)
             } else {
                 base.fill()
             }

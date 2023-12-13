ee76b9e1fc0574361314fedc3284015e036ba533
jsuhu <111390114+jsuhu@users.noreply.github.com>
2023-12-13 16:11:53 -0500

    Lecture 3: struct, class, protocol, View, ViewModel, Model

---
+++ project1/EmojiMemoryGame.swift
@@ -0,0 +1,8 @@
+//
+//  EmojiMemoryGame.swift
+//  project1
+//
+//  Created by Jingyi Su on 12/13/23.
+//
+
+import Foundation
---
+++ project1/MemorizeGame.swift
@@ -0,0 +1,8 @@
+//
+//  MemorizeGame.swift
+//  project1
+//
+//  Created by Jingyi Su on 12/13/23.
+//
+
+import Foundation
